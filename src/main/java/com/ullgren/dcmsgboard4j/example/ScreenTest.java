b6e0ac9a25ccf73ee3f40aa6f54fd985726766cb
Pontus Ullgren <ullgren@gmail.com>
2015-02-16 11:39:04 +0100

    Updated pom.xml and example program to use Java SE 7. Closes #3

--- src/main/java/com/ullgren/dcmsgboard4j/example/ScreenTest.java
+++ src/main/java/com/ullgren/dcmsgboard4j/example/ScreenTest.java
@@ -3,33 +3,30 @@ package com.ullgren.dcmsgboard4j.example;
 import com.ullgren.dcmsgboard4j.*;
 
 public class ScreenTest {
-	
+
 	public static void main(String[] args) {
-//		Message message = new ScrollingTextMessage("Test");
-		Message message = new TestpatternMessage();
-//		Board board = new UsbBoard();
-//		board.open();
-		
-		Board board2 = new ConsoleBoard();
-		board2.open();
-		
-		Screen s = message.getNextScreen();
-		for(int i = 0; s!=null; i++) {
-			
-//			board.update(s);
-			board2.update(s);
-			try {
-				Thread.sleep(200);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			// Only get a new screen every second loop
-			if ( i%2 == 0 ) {
-				s = message.getNextScreen();
+		 Message message = new ScrollingTextMessage("This is generated from a Java program");
+//		Message message = new TestpatternMessage();
+		try (Board board = new UsbBoard(); Board board2 = new ConsoleBoard();) {
+			board.open();
+			board2.open();
+
+			Screen s = message.getNextScreen();
+			for (int i = 0; s != null; i++) {
+
+				board.update(s);
+				board2.update(s);
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				// Only get a new screen every second loop
+				if (i % 2 == 0) {
+					s = message.getNextScreen();
+				}
 			}
 		}
-//		board.close();
-		board2.close();
 	}
 
 }
